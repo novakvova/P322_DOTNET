e95c2c381adc9b9cb95a04ba81d3d6d7a1b7230e
novakvova <novakvova@gmail>
2024-12-12 17:05:07 +0200

    Begin working Generic

---
+++ BeginGeneryc/MyList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginGeneryc
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private Node<T> head; //Голова
+        private Node<T> tail; //Хвіст - останій елемент списку Next == null
+        private int _count;
+
+        public int Count
+        {
+            get { return _count; }
+            private set { _count = value; }
+        }
+
+        public MyList()
+        {
+            head = null;
+            tail = null;
+            Count = 0; 
+        }
+
+        public void Add(T item)
+        {
+            Node<T> temp = new Node<T>(item);
+            if (head == null)
+            {
+                head = temp;
+                tail = temp;
+            }
+            else
+            {
+                tail.Next = temp; //Зберігаємо наступний елемнет у Next
+                tail = temp;      //Наступний елемент стає у хвіст
+            }
+            Count++;
+        }
+
+        public bool Remove(T value)
+        {
+            if (head == null) return false;
+            if (head.Value.Equals(value))
+            {
+                head = head.Next;
+                return true;
+            }
+            var current = head;
+            while (current.Next != null)
+            {
+                if (current.Next.Value.Equals(value))
+                {
+                    current.Next = current.Next.Next;
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                var current = head;
+                int k = 0;
+                while (k != index)
+                {
+                    current = current.Next;
+                    k++;
+                }
+                return current.Value;
+            }
+            set
+            {
+                var current = head;
+                int k = 0;
+                while (k != index)
+                {
+                    current = current.Next;
+                    k++;
+                }
+                current.Value = value;
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public override string ToString()
+        {
+            var current = head;
+            var result = "";
+            while (current != null)
+            {
+                result += current.Value.ToString() + "\t";
+                current = current.Next;   //Переходимо на наступний елемент
+            }
+            return result;
+        }
+    }
+}
---
+++ BeginGeneryc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginGeneryc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Привіт!");
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine(list);
+
+            list.Remove(2);
+            Console.WriteLine("-----Remove-----");
+            //Console.WriteLine(list);
+            foreach (int i in list)
+            {
+                Console.Write($"{i}\t");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
