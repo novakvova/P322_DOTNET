0f75246afcb177fcd5746510e9224a43ce192594
novakvova <novakvova@gmail>
2024-12-12 17:16:53 +0200

    Contact

---
+++ BeginGeneryc/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginGeneryc
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\t Phone: {Phone}";
+        }
+    }
+    
+}
--- BeginGeneryc/Program.cs
+++ BeginGeneryc/Program.cs
@@ -12,21 +12,34 @@ namespace BeginGeneryc
         {
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("Привіт!");
-            MyList<int> list = new MyList<int>();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            Console.WriteLine(list);
-
-            list.Remove(2);
-            Console.WriteLine("-----Remove-----");
+            //Console.WriteLine("Привіт!");
+            //MyList<int> list = new MyList<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
             //Console.WriteLine(list);
-            foreach (int i in list)
+
+            ////Console.WriteLine(list[1]);
+
+            //list.Remove(2);
+            //Console.WriteLine("-----Remove-----");
+            ////Console.WriteLine(list);
+            //foreach (int i in list)
+            //{
+            //    Console.Write($"{i}\t");
+            //}
+            //Console.WriteLine();
+
+            MyList<Contact> contacts = new MyList<Contact>();
+            contacts.Add(new Contact { Name = "Марко Іван", Phone = "+38(096) 23 12 345" });
+            contacts.Add(new Contact { Name = "Мельник Вікторія", Phone = "+38(096) 98 12 315" });
+
+            foreach (Contact contact in contacts)
             {
-                Console.Write($"{i}\t");
+                Console.WriteLine(contact);
             }
-            Console.WriteLine();
+
+            
             
         }
     }
